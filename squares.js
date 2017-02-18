2d2a7ee7069b7d28984ccf52a43133f177676c71
wesdoyle <wesdoyle08@gmail.com>
2017-02-18 11:34:56 -0600

    setup board

--- squares.js
+++ squares.js
@@ -1,7 +1,35 @@
-function setup(){
+var cols = 8;
+var rows = 8;
 
+var colors = [];
+
+function setup(){
+    createCanvas(401, 401);
+    for (var i = 0; i<cols; i++){
+        colors[i] = []
+        for (var j = 0; j<rows; j++){
+            colors[i][j] = floor(random(255));
+        }
+    }
+}
+
+function mousPressed(){
+    for (var i = 0; i < colors.length; i ++){
+        console.log(colors.length)
+    }
 }
 
 function draw(){
+    background(30);
 
+    for (var i = 0; i < cols; i++){
+        for (var j = 0; j < rows; j++) {
+            var x = i * 50;
+            var y = j * 50;
+            fill(colors[i][j])
+            stroke(0);
+            rect(x, y, 50, 50);
+        }
+    }
 }
+
